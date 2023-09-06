c357de0eb740ea312b54c2721867f1fe7d2e17da
Sabino Lopes <sabinolopes007@gmail.com>
2023-09-06 19:46:20 -0300

    Add api key

--- src/helpers/weatherAPI.js
+++ src/helpers/weatherAPI.js
@@ -1,4 +1,4 @@
-const token = ff6de00c291d49ae959124621230609;
+const token = 'ff6de00c291d49ae959124621230609';
 
 export async function searchCities(term) {
   const apiUrl = `http://api.weatherapi.com/v1/search.json?lang=pt&key=${token}&q=${term}`;
